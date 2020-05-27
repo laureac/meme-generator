94fe3b769eaefda2630ce8f49b6ce7c0d5c6acc5
Laure Claret <laure.a.claret@gmail.com>
2020-05-28 09:38:53 +1000

    Updates

---
+++ precache-manifest.138c2ec49b421a57b6800b185ffe96e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1b515ebc2709a903690cbc62e693d06",
+    "url": "/meme-generator/index.html"
+  },
+  {
+    "revision": "f282afc96c20d85cf2b3",
+    "url": "/meme-generator/static/css/main.89806d35.chunk.css"
+  },
+  {
+    "revision": "cef568d202ebe3324052",
+    "url": "/meme-generator/static/js/2.d6828507.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/meme-generator/static/js/2.d6828507.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f282afc96c20d85cf2b3",
+    "url": "/meme-generator/static/js/main.d51ca49c.chunk.js"
+  },
+  {
+    "revision": "64b376743c3fd3988108",
+    "url": "/meme-generator/static/js/runtime-main.338604b4.js"
+  }
+]);
